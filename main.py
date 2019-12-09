f247f3f6afc0a77b62646074740adf6175ac8f39
vyannchen <chenvyann@gmail.com>
2019-12-09 00:09:35 -0500

    open csv file

--- main.py
+++ main.py
@@ -2,6 +2,15 @@
 # Dataset: https://www.kaggle.com/usdot/flight-delays
 # CS 484 Final Project
 
+import pandas as pd
+
+
+def read_csv(csv_file):
+    data = pd.read_csv(csv_file)
+    return data
+
 
 if __name__ == '__main__':
-    print("Main")
+    airlines = read_csv("data/airlines.csv")
+    airports = read_csv("data/airports.csv")
+    flights = read_csv("data/flights.csv")
